f217ab263dbb950e3bb05dc001453813d7a4a2b3
Rickard Englund <rickard.englund@cygni.se>
2021-04-08 21:14:27 +0200

    Print progress, add game setting information

--- cmd/examplebot/main.go
+++ cmd/examplebot/main.go
@@ -11,14 +11,16 @@ import (
 )
 
 func main() {
-	basebot.Start("Simple Go Bot", models.Training, desiredGameSettings, calculateMove)
+	basebot.Start("Simple Bot", models.Training, desiredGameSettings, calculateMove)
 }
 
-var moves = []models.Action{models.Explode, models.Left, models.Down, models.Right, models.Up} //, models.Stay}
-var lastDir = 0
+var (
+	moves   = []models.Action{models.Explode, models.Left, models.Down, models.Right, models.Up} // , models.Stay}
+	lastDir = 0
+)
 
 // Implement your paintbot here
-func calculateMove(updateEvent models.MapUpdateEvent) models.Action {
+func calculateMove(settings models.GameSettings, updateEvent models.MapUpdateEvent) models.Action {
 	utility := maputility.New(updateEvent.Map, *updateEvent.ReceivingPlayerID)
 	me := utility.GetMe()
 	move := models.Stay
--- utilities/maputility/player.go
+++ utilities/maputility/player.go
@@ -3,36 +3,34 @@ package maputility
 import "paintbot-client/models"
 
 type Player struct {
-	info    models.CharacterInfo
+	info    *models.CharacterInfo
 	utility *MapUtility
 }
 
-func (p *Player) GetPos() models.Coordinates {
+func (p Player) GetPos() models.Coordinates {
 	return p.utility.ConvertPositionToCoordinates(p.info.Position)
 }
 
-func (p *Player) HasPowerUp() bool {
+func (p Player) HasPowerUp() bool {
 	return p.info.CarryingPowerUp
 }
 
-func (p * Player) StunnedForTicks() int {
+func (p Player) StunnedForTicks() int {
 	return p.info.StunnedForGameTicks
 }
 
-func (p * Player) GetPoints() int {
+func (p Player) GetPoints() int {
 	return p.info.Points
 }
 
-func (p * Player) GetName() string {
+func (p Player) GetName() string {
+	return p.info.Name
+}
+
+func (p Player) GetID() string {
 	return p.info.ID
 }
 
-func (p * Player) GetColouredPositions() []models.Coordinates {
-	coords := make([]models.Coordinates, len(p.info.ColouredPosition))
-	for i := range coords {
-		coords[i] = p.utility.ConvertPositionToCoordinates(p.info.ColouredPosition[i])
-	}
-
-	return coords
+func (p Player) GetColouredPositions() []models.Coordinates {
+	return p.utility.ConvertPositionsToCoordinates(p.info.ColouredPosition)
 }
-
